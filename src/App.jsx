ea3d7483f86c3da0063bce9e997f95ec7c731c03
Patcharin-T <Patcharinn.tl@gmail.com>
2025-11-16 09:35:03 +0700

    Taskapp จัดการงาน

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './screens/Home'
+import ShowAllTask from './screens/ShowAllTask'
+import AddTask from './screens/AddTask'
+import UpdateTask from './screens/UpdateTask'
+export default function App() {
+  return (
+    <>
+     <BrowserRouter>
+     <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/showalltask' element={<ShowAllTask/>}/>
+        <Route path='/addtask' element={<AddTask/>}/>
+        <Route path='/updatetask/:id' element={<UpdateTask/>}/>
+     </Routes>
+     </BrowserRouter> 
+    </>
+  )
+}
